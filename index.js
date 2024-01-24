a09d3f43c9040bbc920724902ffd1fefbd24809d
klearliu <clearlynetworking@gmail.com>
2024-01-24 01:18:56 -0700

    assignment 2 setup files

---
+++ index.js
@@ -0,0 +1,10 @@
+// main.js
+
+const {add,subtract,multiply,divide} = require('./mathFunctions');
+const {message} = require('./message');
+
+console.log(add(4, 5));
+console.log(subtract(10, 2));
+console.log(multiply(4, 5));
+console.log(divide(20, 5));
+message();
---
+++ message.js
@@ -0,0 +1,5 @@
+function message(){
+    console.log("Hello there noob");
+}
+
+module.exports = {message};
